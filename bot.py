6f53ad15e768e833aef8cb4b69b2e2c00e02d3b4
HaRmX96 <97217967+HaRmX96@users.noreply.github.com>
2022-06-06 15:40:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from typing import Tuple
 
 class Client():
 	run=False
-	def __init__(self, connect:Tuple[str,int]=("10.0.0.45",9999)) -> None:
+	def __init__(self, connect:Tuple[str,int]=("your ipv4",9999)) -> None:
 		signal.signal(signal.SIGINT, self.exit_gracefully)
 		signal.signal(signal.SIGTERM, self.exit_gracefully)
 		self.stop = False
